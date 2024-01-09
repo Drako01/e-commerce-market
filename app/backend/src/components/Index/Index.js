88daa54b1da7202765f7f8cbcfa05179137a2ccc
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-09 13:08:28 -0300

    Implementando Edit User

--- app/backend/src/components/Index/Index.js
+++ app/backend/src/components/Index/Index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import logo from '../../logo.svg'
 import './Index.css';
 
 const Index = () => {
+    const urlServer = process.env.REACT_APP_URL_SERVER;
     const auth = getAuth();
     const [currentUser, setCurrentUser] = useState(null);
     const [selectedUserDetails, setSelectedUserDetails] = useState(null);
-    const [showDetailsModal, setShowDetailsModal] = useState(false);
-
+    const [showDetailsModal, setShowDetailsModal] = useState(false);    
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [editingUser, setEditingUser] = useState({
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+    });
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -35,6 +44,43 @@ const Index = () => {
         setShowDetailsModal(true);
     };
 
+    const handleEditUser = (user) => {
+        setEditingUser(user);
+        setShowEditModal(true);
+        setShowDetailsModal(false);
+    };
+
+    //Cambiar por la que esta en Signup
+    const handleSaveChanges = async () => {
+        try {
+            const updatedUserData = {
+                displayName: editingUser.displayName,
+                photoURL: editingUser.photoURL, 
+            };
+
+            const response = await fetch(`${urlServer}/users/${editingUser.uid}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedUserData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+
+            setShowEditModal(false);            
+        } catch (error) {
+            console.error('Error al guardar cambios:', error.message);
+            // Puedes manejar otros errores aquí si es necesario
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Error al guardar los cambios del usuario',
+            });
+        }
+    };
 
     return (
         <div className="App-index container inicio">
@@ -72,8 +118,46 @@ const Index = () => {
                     <Button variant="secondary" onClick={() => setShowDetailsModal(false)}>
                         Cerrar
                     </Button>
-                    <Button variant="primary" onClick={() => setShowDetailsModal(false)}>
+                    <Button variant="primary" onClick={() => handleEditUser(currentUser)}>
                         Editar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {/* Modal para editar usuarios */}
+            <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Editar Usuario</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formEmailEdit">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Ingrese el correo electrónico"
+                                value={editingUser.email}
+                                readOnly // Cambiado a readOnly en lugar de disabled
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formDisplayNameEdit">
+                            <Form.Label>Nombre:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Ingrese el nombre"
+                                value={editingUser.displayName}
+                                onChange={(e) => setEditingUser({ ...editingUser, displayName: e.target.value })}
+                            />
+                        </Form.Group>
+                        
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowEditModal(false)}>
+                        Cerrar
+                    </Button>
+                    <Button variant="primary" onClick={handleSaveChanges}>
+                        Guardar Cambios
                     </Button>
                 </Modal.Footer>
             </Modal>
