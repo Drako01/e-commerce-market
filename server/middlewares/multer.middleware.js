af1c50cbbee1945ba503a674284a3497628b8400
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-09 01:15:50 -0300

    Multer mejorado

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -3,11 +3,19 @@ import multer from 'multer';
 // Configuración de Multer
 const storage = multer.memoryStorage();
 
+const imageFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('Solo se permiten archivos de imagen'), false);
+    }
+    cb(null, true);
+};
+
 const multerUpload = multer({
     storage: storage,
     limits: {
-        fileSize: 5 * 1024 * 1024, 
+        fileSize: 5 * 1024 * 1024,
     },
+    fileFilter: imageFilter,
 });
 
 export { multerUpload };
