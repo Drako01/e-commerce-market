19ee9e6d2a1205052f93f4a59c9d505c8337eaf1
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-10 18:42:03 -0300

    Productos Verdaderos de la BD

--- server/models/products.js
+++ server/models/products.js
@@ -1,34 +1,69 @@
-export const products = [{
-        uid: 1,
-        marca: 'Ejemplo1',
-        subcategoria: 'Subcategoría1',
-        categoria: 'Categoría1',
-        precio: 10.0,
-        descripcion: 'Descripción del producto 1',
-        foto: 'https://estudiokobe.com/wp-content/uploads/Fotografia-de-producto-buenos-aires-argentina-zapatos-calzado-femenino-e-commerce-7-1-768x680.jpg',
-        stock: 100,
-        photoUrl: '',
+export const products = [
+    {
+        "id": "0LIq9bslTvvbBL5of39A",
+        "subcategoria": "Dulce de Leche",
+        "precio": "3099.25",
+        "stock": "1500",
+        "descripcion": "Dulce de Leche por 400gr - Estilo Colonial",
+        "categoria": "Alimentos",
+        "marca": "La Serenisima",
+        "imageUrl": "Products%2FLa%20Serenisima%2FAlimentos%2FDulce%20de%20Leche%2F000Z-006-003-02592497Z-006-003-025-DDLColonialLaSerenisima.png?alt=media&token=2c56f9e3-a018-4678-874a-8dcbb31825a7",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FLa%20Serenisima%2FAlimentos%2FDulce%20de%20Leche%2F000Z-006-003-02592497Z-006-003-025-DDLColonialLaSerenisima.png?alt=media&token=2c56f9e3-a018-4678-874a-8dcbb31825a7"
     },
     {
-        uid: 2,
-        marca: 'Ejemplo2',
-        subcategoria: 'Subcategoría2',
-        categoria: 'Categoría2',
-        precio: 20.0,
-        descripcion: 'Descripción del producto 2',
-        foto: 'https://estudiokobe.com/wp-content/uploads/fotografia-de-producto-argentina-accesorios-carteras-2-1-768x768.jpg',
-        stock: 200,
-        photoUrl: '',
+        "id": "Ibl9WiMDZ342Y2AlFNg2",
+        "imageUrl": "Products%2FSamsung%2FElectr%C3%B3nica%2FSmartphones%2FD_NQ_NP_899252-MLA54334212764_032023-O.webp?alt=media&token=c6a27b26-ddd6-4988-a8ff-d0e46ae03323",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FSamsung%2FElectr%C3%B3nica%2FSmartphones%2FD_NQ_NP_899252-MLA54334212764_032023-O.webp?alt=media&token=c6a27b26-ddd6-4988-a8ff-d0e46ae03323",
+        "stock": "200",
+        "subcategoria": "Smartphones",
+        "marca": "Samsung",
+        "descripcion": "Celular Samsung Galaxy S22 Ultra 5g 256 Gb Phantom",
+        "categoria": "Electrónica",
+        "precio": "1649990"
     },
     {
-        uid: 3,
-        marca: 'Ejemplo3',
-        subcategoria: 'Subcategoría3',
-        categoria: 'Categoría3',
-        precio: 20.0,
-        descripcion: 'Descripción del producto 2',
-        foto: 'https://img.global.news.samsung.com/ar/wp-content/uploads/2023/01/Samsung_New_Screens_dl2-845x563.jpg',
-        stock: 200,
-        photoUrl: '',
+        "id": "dofa7ujHIfsOl9oneywv",
+        "marca": "Terrabusi",
+        "stock": "5000",
+        "imageUrl": "Products%2FTerrabusi%2FAlimentos%2FGalletitas%20Dulces%2Fterrabusi.png?alt=media&token=d2c7a625-4043-44ef-854a-d134ebe91ce2",
+        "categoria": "Alimentos",
+        "descripcion": "Galletitas Dulces Surtidas - Divertidas",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FTerrabusi%2FAlimentos%2FGalletitas%20Dulces%2Fterrabusi.png?alt=media&token=d2c7a625-4043-44ef-854a-d134ebe91ce2",
+        "subcategoria": "Galletitas Dulces",
+        "precio": "1973.40"
     },
-]
+    {
+        "id": "jdFgOSOqYb3ZKFAtP3BH",
+        "categoria": "Electrodomésticos",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FLG%2FElectrodom%C3%A9sticos%2FTelevisores%2FD_NQ_NP_646729-MLA51185421870_082022-O.webp?alt=media&token=2852c4f7-e446-4cc0-81df-06e03d064f52",
+        "precio": "3299998.90",
+        "subcategoria": "Televisores",
+        "descripcion": "Smart TV LG AI ThinQ 60UP7750PSB LCD 4K 60\" 100V/240V",
+        "imageUrl": "Products%2FLG%2FElectrodom%C3%A9sticos%2FTelevisores%2FD_NQ_NP_646729-MLA51185421870_082022-O.webp?alt=media&token=2852c4f7-e446-4cc0-81df-06e03d064f52",
+        "stock": "50",
+        "marca": "LG"
+    },
+    {
+        "id": "p3oQvVV8PnJsaGSCYIYE",
+        "precio": "1454.75",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FAlhild%2FAlimentos%2FYerba%20Mate%2Fpaquete-new.png?alt=media&token=4b7987b8-c762-41dd-81e5-e56d6c54f060",
+        "subcategoria": "Yerba Mate",
+        "imageUrl": "Products/Alhild/Alimentos/Yerba Mate/undefined",
+        "marca": "Alhild",
+        "stock": "1300",
+        "categoria": "Alimentos",
+        "descripcion": "Yerba Mate Orgánica"
+    },
+    {
+        "id": "uuiodBWkfPHdnFFfL7JW",
+        "subcategoria": "Azucar",
+        "categoria": "Alimentos",
+        "marca": "Ledezma",
+        "imageUrl": "Products%2FLedezma%2FAlimentos%2FAzucar%2FLEDESMA-azucar-x1Kg-WEB.png?alt=media&token=15798d9f-6bf1-4372-9d4e-c5cb89063ea1",
+        "stock": "16500",
+        "precio": "1563.54",
+        "foto": "https://firebasestorage.googleapis.com/v0/b/e-commerce-backend-ae6f6.appspot.com/o/Products%2FLedezma%2FAlimentos%2FAzucar%2FLEDESMA-azucar-x1Kg-WEB.png?alt=media&token=15798d9f-6bf1-4372-9d4e-c5cb89063ea1",
+        "descripcion": "Azúcar Blanca"
+    }
+];
+
